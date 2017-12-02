0709801a82edc8d5511ec065b5044869417301e2
Jake Downs <mail@jakedowns.com>
2017-12-02 17:16:56 -0500

    spaces -> tabs

--- RobloxAPIService.php
+++ RobloxAPIService.php
@@ -16,21 +16,21 @@ class RobloxAPIService
 
 	public function __construct(){
 		$this->curlMulti = new CurlMultiHandler();
-        $this->client = $this->_make_client();
+		$this->client = $this->_make_client();
 	}
 	
 	/**
-
-		This method calculates a total recent average price
-		for a given user by looping over the RAP value of 
-		each collectible asset a user owns.
-
-		For each available asset type,
-		this script polls the Roblox Web API
-		and continues to poll for each asset type for as many pages
-		as necessary by checking for the existence of 
-		the next_page_cursor property
-
+	*
+	*	This method calculates a total recent average price
+	*	for a given user by looping over the RAP value of 
+	*	each collectible asset a user owns.
+	*
+	* 	For each available asset type,
+	*	this script polls the Roblox Web API
+	*	and continues to poll for each asset type for as many pages
+	*	as necessary by checking for the existence of 
+	*	the next_page_cursor property
+	*
 	**/
 	public function get_total_rap_for_user($user_id)
 	{
@@ -38,29 +38,29 @@ class RobloxAPIService
 		$limiteds = $this->get_all_limiteds_concurrent();
 
 		$total_pages = 0;
-        $total_rap = 0;
-        $total_rap_avg = 0;
-        $total_limiteds = 0;
+		$total_rap = 0;
+		$total_rap_avg = 0;
+		$total_limiteds = 0;
 
-        foreach($limiteds as $assetType => $assetsForType){
-        	$total_pages += $assetsForType->pages;
-            foreach($assetsForType->items as $asset){
-                $total_limiteds++;
-                $total_rap += floatval($asset->recentAveragePrice);
-            }
-        }
+		foreach($limiteds as $assetType => $assetsForType){
+			$total_pages += $assetsForType->pages;
+			foreach($assetsForType->items as $asset){
+				$total_limiteds++;
+				$total_rap += floatval($asset->recentAveragePrice);
+			}
+		}
 
-        if($total_limiteds){
-            $total_rap_avg = $total_rap / $total_limiteds;
-        }
+		if($total_limiteds){
+			$total_rap_avg = $total_rap / $total_limiteds;
+		}
 
-        return (object) [
-            'total_pages' => $total_pages,
-            'total_limiteds' => $total_limiteds,
-            'total_rap' => $total_rap,
-            'total_rap_avg' => $total_rap_avg,
-            'limiteds_by_asset_type' => $limiteds
-        ];
+		return (object) [
+			'total_pages' => $total_pages,
+			'total_limiteds' => $total_limiteds,
+			'total_rap' => $total_rap,
+			'total_rap_avg' => $total_rap_avg,
+			'limiteds_by_asset_type' => $limiteds
+		];
 	}
 
 	private function get_all_limiteds_concurrent()
@@ -72,12 +72,12 @@ class RobloxAPIService
 				if(isset($single_result['value']->data)){
 					$data = $single_result['value']->data;
 				}
-                $results->{$single_result['value']->assetType} = (object)[
-                    'total_items' => count($data),
-                    'pages' => $single_result['value']->pages,
-                    'items'       => $data
-                ];
-            }
+				$results->{$single_result['value']->assetType} = (object)[
+					'total_items' => count($data),
+					'pages' => $single_result['value']->pages,
+					'items'       => $data
+				];
+			}
 			return $results;
 		});
 
@@ -89,136 +89,136 @@ class RobloxAPIService
 	private function _make_client(){
 		$curlMulti = $this->curlMulti;
 		$stack = HandlerStack::create($curlMulti);
-	    $opts = [
-	    	'handler' => $stack,
-	        'verify' => false,
-	        'base_uri' => '',
-	        'allow_redirects' => true,
-	        'timeout' => 0,
-	        'http_errors' => false,
-	        'headers' => [
-	          'Cache-Control' => 'no-cache'
-	        ]
-	    ];
+		$opts = [
+			'handler' => $stack,
+			'verify' => false,
+			'base_uri' => '',
+			'allow_redirects' => true,
+			'timeout' => 0,
+			'http_errors' => false,
+			'headers' => [
+			  'Cache-Control' => 'no-cache'
+			]
+		];
 
-	    $client = new GuzzleHttp\Client($opts);
-	    return $client;
+		$client = new GuzzleHttp\Client($opts);
+		return $client;
 	}
 
 	private function getAssetTypePromises()
 	{
 		$promises = [];
-        foreach ($this->assetTypes as $assetType) {
-            $reqParams = (object) [
-                'userId' => $this->user_id,
-                'assetType' => $assetType
-            ];
+		foreach ($this->assetTypes as $assetType) {
+			$reqParams = (object) [
+				'userId' => $this->user_id,
+				'assetType' => $assetType
+			];
 
-            $finalOuterPromise = new Promise();
-            $ignorablePromise = $this->getUserAssetCollectiblesByAssetTypePromisified($this->client, $reqParams, $finalOuterPromise);
-            $promises[] = $finalOuterPromise;
-        }
-        return $promises;
+			$finalOuterPromise = new Promise();
+			$ignorablePromise = $this->getUserAssetCollectiblesByAssetTypePromisified($this->client, $reqParams, $finalOuterPromise);
+			$promises[] = $finalOuterPromise;
+		}
+		return $promises;
 	}
 
 	private function getUserAssetCollectiblesByAssetTypePromisified($client, $req, $finalOuterPromise){
 		$req_url_base = str_replace('USER_ID', $req->userId, $this->url_user_asset_collectibles);
 		$query_obj = [
-            'assetType' => $req->assetType,
-            'sortOrder' => isset($req->sortOrder) ? $req->sortOrder : 'Asc',
-            'limit' => isset($req->limit) ? $req->limit : '100',
-            'cursor' => isset($req->cursor) ? $req->cursor : ''
-        ];
+			'assetType' => $req->assetType,
+			'sortOrder' => isset($req->sortOrder) ? $req->sortOrder : 'Asc',
+			'limit' => isset($req->limit) ? $req->limit : '100',
+			'cursor' => isset($req->cursor) ? $req->cursor : ''
+		];
 
-        return $this->getAllResultPagesPromise(
-            $req_url_base,
-            $query_obj,
-            'Roblox_Collectibles',
-            function($result_decoded){
-                // get_next_page_cursor
-                //dd(['get_next_page_cursor', $result_decoded]);
-                if(isset($result_decoded->nextPageCursor)){
-                    return $result_decoded->nextPageCursor;
-                }
+		return $this->getAllResultPagesPromise(
+			$req_url_base,
+			$query_obj,
+			'Roblox_Collectibles',
+			function($result_decoded){
+				// get_next_page_cursor
+				//dd(['get_next_page_cursor', $result_decoded]);
+				if(isset($result_decoded->nextPageCursor)){
+					return $result_decoded->nextPageCursor;
+				}
 
-                return null;
-            },
-            function($result_decoded){
-                // fn_get_nested_data
-                if(isset($result_decoded->data)){
-                    return $result_decoded->data;
-                }
+				return null;
+			},
+			function($result_decoded){
+				// fn_get_nested_data
+				if(isset($result_decoded->data)){
+					return $result_decoded->data;
+				}
 
-                return [];
-            },
-            $client,
-            $finalOuterPromise
-        );
+				return [];
+			},
+			$client,
+			$finalOuterPromise
+		);
 	}
 
 	public function getAllResultPagesPromise(
-        $req_url_base,
-        $query_obj,
-        $type,
-        $fn_get_next_page_cursor,
-        $fn_get_nested_data,
-        $client,
-        $finalOuterPromise
-    ){
-        return (new UserInventoryWorker())->getAsPromise(
-            $req_url_base,
-            $query_obj,
-            $type,
-            $fn_get_next_page_cursor,
-            $fn_get_nested_data,
-            $client,
-            $finalOuterPromise
-        );
-    }
+		$req_url_base,
+		$query_obj,
+		$type,
+		$fn_get_next_page_cursor,
+		$fn_get_nested_data,
+		$client,
+		$finalOuterPromise
+	){
+		return (new UserInventoryWorker())->getAsPromise(
+			$req_url_base,
+			$query_obj,
+			$type,
+			$fn_get_next_page_cursor,
+			$fn_get_nested_data,
+			$client,
+			$finalOuterPromise
+		);
+	}
 
-    public function waitForPromiseGroup($promises, $foldResults_fn) {
-      $this->settled = false;
-      $results = (object) [];
+	public function waitForPromiseGroup($promises, $foldResults_fn) {
+	  $this->settled = false;
+	  $results = (object) [];
 
-      $group = \GuzzleHttp\Promise\settle($promises)->then(function ($promise_results) use (&$results, $foldResults_fn) {
-        $this->settled = true;
-        try {
-          foreach ($promise_results as $result) {
-            $results = call_user_func($foldResults_fn, $results, $result);
-          }
-        }catch(\Exception $e){
-          //dd($e);
-        }
-      });
+	  $group = \GuzzleHttp\Promise\settle($promises)->then(function ($promise_results) use (&$results, $foldResults_fn) {
+		$this->settled = true;
+		try {
+		  foreach ($promise_results as $result) {
+			$results = call_user_func($foldResults_fn, $results, $result);
+		  }
+		}catch(\Exception $e){
+		  //dd($e);
+		}
+	  });
 
-      // Debugging loop, ticks in background
-      // if Promises don't resolve within MAX_TICKS, we exit early
-      define('MAX_TICKS_BEFORE_SCRIPT_BREAK', 5000);
-      $ticks = 0;
-      $broken = false;
-      while ($group->getState() === 'pending' && ! $broken) {
-        $this->curlMulti->tick();
-        //\GuzzleHttp\Promise\queue()->run();
+	  // Debugging loop, ticks in background
+	  // if Promises don't resolve within MAX_TICKS, we exit early
+	  define('MAX_TICKS_BEFORE_SCRIPT_BREAK', 5000);
+	  $ticks = 0;
+	  $broken = false;
+	  while ($group->getState() === 'pending' && ! $broken) {
+		$this->curlMulti->tick();
+		//\GuzzleHttp\Promise\queue()->run();
 
-        // just in case, limit to N ticks...
-        $ticks++;
-        if($ticks >= MAX_TICKS_BEFORE_SCRIPT_BREAK
-          // or empty queue detected
-          // || \GuzzleHttp\Promise\queue()->isEmpty()
-        ){
+		// just in case, limit to N ticks...
+		$ticks++;
+		if($ticks >= MAX_TICKS_BEFORE_SCRIPT_BREAK
+		  // or empty queue detected
+		  // || \GuzzleHttp\Promise\queue()->isEmpty()
+		){
 		  print("<br/>script broke");
-          $broken = true;
-          break;
-        }
-        usleep(1);
-      }
+		  $broken = true;
+		  break;
+		}
+		usleep(1);
+	  }
 
-      // DEBUG Helper
-      // print("<br/>waitForPromiseGroup ran for $ticks ticks");
-      // foreach($promises as $promise){
-      // 	print("<br>" . $promise->getState());
-      // }
+	  // DEBUG Helper
+	  // print("<br/>waitForPromiseGroup ran for $ticks ticks");
+	  // foreach($promises as $promise){
+	  // 	print("<br>" . $promise->getState());
+	  // }
 
-      return $results;
-    }
+	  return $results;
+	}
 }
--- UserInventoryWorker.php
+++ UserInventoryWorker.php
@@ -10,134 +10,134 @@ use function GuzzleHttp\Promise\promise_for;
 class UserInventoryWorker
 {
 	public function getAsPromise(
-        $req_url_base,
-        $query_obj,
-        $type,
-        $fn_get_next_page_cursor,
-        $fn_get_nested_data,
-        $client,
-        $finalOuterPromise
-    ){
-        $this->req_url_base = $req_url_base;
-        $this->query_obj = $query_obj;
-        $this->type = $type;
-        $this->fn_get_next_page_cursor = $fn_get_next_page_cursor;
-        $this->fn_get_nested_data = $fn_get_nested_data;
-        $this->client = $client;
-        $this->full_results = [];
-        $this->page_number = 1;
-        $this->pages_remaining = true;
-        $this->pages_resolved = [];
-        $this->finalOuterPromise = $finalOuterPromise;
+		$req_url_base,
+		$query_obj,
+		$type,
+		$fn_get_next_page_cursor,
+		$fn_get_nested_data,
+		$client,
+		$finalOuterPromise
+	){
+		$this->req_url_base = $req_url_base;
+		$this->query_obj = $query_obj;
+		$this->type = $type;
+		$this->fn_get_next_page_cursor = $fn_get_next_page_cursor;
+		$this->fn_get_nested_data = $fn_get_nested_data;
+		$this->client = $client;
+		$this->full_results = [];
+		$this->page_number = 1;
+		$this->pages_remaining = true;
+		$this->pages_resolved = [];
+		$this->finalOuterPromise = $finalOuterPromise;
 
-        $this->result_promise = $this->getResultsPage(
-            $this->req_url_base,
-            $this->query_obj,
-            $this->type,
-            $this->client
-        );
+		$this->result_promise = $this->getResultsPage(
+			$this->req_url_base,
+			$this->query_obj,
+			$this->type,
+			$this->client
+		);
 
-        $onPageFulfilled = $this->makeOnPageFulfilled();
-        $this->result_promise->then($onPageFulfilled);
+		$onPageFulfilled = $this->makeOnPageFulfilled();
+		$this->result_promise->then($onPageFulfilled);
 
-        return $this->result_promise;
-    }
+		return $this->result_promise;
+	}
 
-    public function getResultsPage($req_url_base, $query_obj, $type, $client){
-        $query = http_build_query($query_obj);
-        $req_url = $req_url_base . '?' . $query;
+	public function getResultsPage($req_url_base, $query_obj, $type, $client){
+		$query = http_build_query($query_obj);
+		$req_url = $req_url_base . '?' . $query;
 
-        //print_r(['getResultsPage', $req_url]);
+		//print_r(['getResultsPage', $req_url]);
 
-        if(!$client || !isset($client)){
-            die('client missing');
-        }
+		if(!$client || !isset($client)){
+			die('client missing');
+		}
 
-        return $client->getAsync($req_url);
-    }
+		return $client->getAsync($req_url);
+	}
 
-    public function makeOnPageFulfilled(){
-        $OPF = function($response){
-            // dump('HereA');
+	public function makeOnPageFulfilled(){
+		$OPF = function($response){
+			// dump('HereA');
 
-            //print_r(['on page fulfilled',$response]);
+			//print_r(['on page fulfilled',$response]);
 
-            $result_decoded = $this->decodeResponse($response);
+			$result_decoded = $this->decodeResponse($response);
 
-            // dump('HereB');
+			// dump('HereB');
 
-            $this->full_results = array_merge($this->full_results, call_user_func($this->fn_get_nested_data,$result_decoded));
+			$this->full_results = array_merge($this->full_results, call_user_func($this->fn_get_nested_data,$result_decoded));
 
-            // dump('HereC');
+			// dump('HereC');
 
-            $next_page_cursor = call_user_func($this->fn_get_next_page_cursor,$result_decoded);
+			$next_page_cursor = call_user_func($this->fn_get_next_page_cursor,$result_decoded);
 
-            // dump(['HereD', $next_page_cursor]);
+			// dump(['HereD', $next_page_cursor]);
 
-            if($next_page_cursor) {
-                $this->page_number++;
+			if($next_page_cursor) {
+				$this->page_number++;
 
-                // update $query_obj with new cursor for next while iteration
-                $this->query_obj['cursor'] = $next_page_cursor;
-                $this->query_obj['pageNumber'] = $this->page_number;
+				// update $query_obj with new cursor for next while iteration
+				$this->query_obj['cursor'] = $next_page_cursor;
+				$this->query_obj['pageNumber'] = $this->page_number;
 
-                //dump(['loading next page of results...', $this->query_obj]);
-                //\Log::info(var_export(['loading next page of results...', $this->query_obj], true));
+				//dump(['loading next page of results...', $this->query_obj]);
+				//\Log::info(var_export(['loading next page of results...', $this->query_obj], true));
 
-                $next_promise = $this->getResultsPage(
-                        $this->req_url_base,
-                        $this->query_obj,
-                        $this->type,
-                        $this->client
-                    );
-                $myOnPageFulfilled = $this->makeOnPageFulfilled(); //call_user_func($this->makeOnPageFulfilled);
-                //dump($myOnPageFulfilled);
-                $next_promise->then($myOnPageFulfilled);
-                $this->result_promise->resolve($next_promise);
-            }else {
-                //dump(['all pages fetched', $this->page_number, $next_page_cursor]);
+				$next_promise = $this->getResultsPage(
+						$this->req_url_base,
+						$this->query_obj,
+						$this->type,
+						$this->client
+					);
+				$myOnPageFulfilled = $this->makeOnPageFulfilled(); //call_user_func($this->makeOnPageFulfilled);
+				//dump($myOnPageFulfilled);
+				$next_promise->then($myOnPageFulfilled);
+				$this->result_promise->resolve($next_promise);
+			}else {
+				//dump(['all pages fetched', $this->page_number, $next_page_cursor]);
 //                \Log::info(var_export(['all pages fetched',
 //                                          $this->query_obj,
 //                                          $this->page_number,
 //                                          $next_page_cursor,
 //                                          count($this->full_results)],
 //                                      true));
-                $this->pages_remaining = false;
+				$this->pages_remaining = false;
 
-                $resolution_object = (object) [
-                  'data' => $this->full_results
-                ];
-                if(isset($this->query_obj['assetType'])){
-                  $resolution_object->{'assetType'} = $this->query_obj['assetType'];
-                }
-                $resolution_object->{'pages'} = $this->page_number; 
-                $this->finalOuterPromise->resolve($resolution_object);
-            }
+				$resolution_object = (object) [
+				  'data' => $this->full_results
+				];
+				if(isset($this->query_obj['assetType'])){
+				  $resolution_object->{'assetType'} = $this->query_obj['assetType'];
+				}
+				$resolution_object->{'pages'} = $this->page_number; 
+				$this->finalOuterPromise->resolve($resolution_object);
+			}
 
-            \GuzzleHttp\Promise\queue()->run();
-        };
-        return $OPF;
-    }
+			\GuzzleHttp\Promise\queue()->run();
+		};
+		return $OPF;
+	}
 
-    public function decodeResponse($response){
-        $response_raw = (string) $response->getBody();
-        $type = isset($this->type) ? $this->type : 'Unknown';
+	public function decodeResponse($response){
+		$response_raw = (string) $response->getBody();
+		$type = isset($this->type) ? $this->type : 'Unknown';
 
-        $response_decoded = false;
-        try{
-            $response_decoded = json_decode($response_raw);
-        }catch(\Exception $e) {
-            $response_decoded = (object) [
-                $type . '_Query_Error' => $e->getMessage()
-            ];
-        }
-        if(!$response_decoded){
-            $response_decoded = (object) [
-                $type . '_Query_Error' => 'Unknown Error: ' . $response_raw
-            ];
-        }
-        return $response_decoded;
-    }
+		$response_decoded = false;
+		try{
+			$response_decoded = json_decode($response_raw);
+		}catch(\Exception $e) {
+			$response_decoded = (object) [
+				$type . '_Query_Error' => $e->getMessage()
+			];
+		}
+		if(!$response_decoded){
+			$response_decoded = (object) [
+				$type . '_Query_Error' => 'Unknown Error: ' . $response_raw
+			];
+		}
+		return $response_decoded;
+	}
 }
 
 ?>
